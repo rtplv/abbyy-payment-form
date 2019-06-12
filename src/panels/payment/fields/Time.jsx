2e00176f0f50aa0f86131b0d3ab8859f954cd717
rtplv <rtplv@yandex.ru>
2019-06-12 17:08:04 +0400

    Time component ready

--- src/panels/payment/fields/Time.jsx
+++ src/panels/payment/fields/Time.jsx
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+
+import './Time.scss';
 
 import { FormRow } from 'components/ui';
 
-class PaymentTime extends Component {
+class PaymentTime extends PureComponent {
   static propTypes = {};
 
   render() {
     return (
       <FormRow className="payment-repeat-time"
                title="Время">
-
+        <input className="payment-repeat-time__field"
+               type="time"
+               onChange={this.handleTimeChange} />
+        <span className="payment-repeat-time__description">
+          (+4 UTC, время московское)
+        </span>
       </FormRow>
     );
+  }
+
+  handleTimeChange = e => {
+    const { value } = e.target;
+    console.log(value);
   }
 }
 
